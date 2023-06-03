042a03c5988b90d7c4289c371b4b851a178c615f
서민우 <alsdn638@gmail.com>
2023-06-04 03:51:09 +0900

    fixed kick.py

--- kick.py
+++ kick.py
@@ -23,25 +23,17 @@ with open(f"midis/{name}.json") as file:
     notes = data["tracks"][1]["notes"]
     duration = data["duration"]
 
-bar_duration = 60/bpm*16
 last_time = 0
 
-max_note = max(notes, key=lambda x: x["midi"])["midi"]
-min_note = min(notes, key=lambda x: x["midi"])["midi"]
-middle = (max_note+min_note)//2
-height = 10
-dist = max_note-min_note
-
-# screen_width = 1280
-# screen_height = 720
-screen_width = bar_duration*100+210
-screen_height = min(dist*height+100, 720)
+screen_width = 480
+screen_height = 480
+# screen_width = bar_duration*100+210
+# screen_height = min(dist*height+100, 720)
 screen = pygame.Surface((screen_width, screen_height))
 # screen = pygame.display.set_mode((screen_width, screen_heigzht))
 
-
 for note in notes:
-    note["anim"] = 25
+    note["anim"] = 35
 
 counter = 0 
 
@@ -53,9 +45,6 @@ os.mkdir("temp_images_folder")
 
 # start_time = pygame.time.get_ticks()
 play_time = 0
-offset = 0
-offset_vel = 0
-scroll = -bar_duration*100
 
 running = True
 while running:
@@ -64,32 +53,12 @@ while running:
 
     for note in notes:
         if play_time >= note["time"]:
-            if bar_duration*100-10 >= note["time"]*100-scroll >= -10:
-                note["anim"] *= 0.77
-                pygame.draw.rect(screen, "#000000", 
-                    [
-                        note["time"]*100+100-note["anim"]/4-scroll-offset,
-                        (middle-note["midi"])*height+screen_height/2,
-                        note["duration"]*100-note["anim"]/6,
-                        10
-                    ]
-                )
-    
-    if play_time >= last_time:
-        last_time += bar_duration
-        offset = 100 
-        scroll += bar_duration*100
-
-    if play_time >= last_time-bar_duration/15.7:
-        offset -= offset_vel/20
-        offset_vel *= 1.7
-    else:
-        offset_vel = 1
-
-    offset *= 0.9
+            note["anim"] *= 0.73
+            if int(note["anim"]) > 0:
+                pygame.draw.rect(screen, "#000000", [40+note["anim"]/2, 40+note["anim"]/2, 400-note["anim"], 400-note["anim"]], int(note["anim"]))
 
     # print(duration-play_time+screen_width/2/100)
-    if duration-play_time < 0:
+    if duration-play_time < -10:
         running = False
         
     counter += 1
